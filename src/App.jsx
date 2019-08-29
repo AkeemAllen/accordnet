8e1409a2a78d71d504741fdb5cdc3c12996be756
Akeem Allen <allenakeem8@protonmail.com>
2019-08-29 13:58:55 -0700

    Modified Registration Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
-import CardComponent from './components/CardComponent.jsx';
-import {ColorPalette, Display} from 'components/ColorPalette';
-import {Login, Register} from './components/login';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
+import {Register} from './components/login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingPage />
+        <Register />
       </div>
     );
   }
--- src/components/login/register.jsx
+++ src/components/login/register.jsx
@@ -1,32 +1,82 @@
 import React, {Component} from 'react';
 import loginImg from 'assets/loginImg.svg';
-
+import axios from 'axios';
 export class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: null,
+      email: null,
+      password: null,
+    };
+  }
+
+  handleSubmit() {
+    const {username, password, email} = this.state;
+    axios.post('https://localhost:5001/api/auth/register', {
+      username,
+      password,
+      email,
+    });
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    const {name, value} = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     return (
-      <div className="base.container" ref={this.props.containerRef}>
-        <div className="header">Register</div>
+      <div className="base-container" ref={this.props.containerRef}>
+        {/* <div className="header">Register</div> */}
         <div className="content">
-          <div className="image">
+          {/* <div className="image">
             <img src={loginImg} alt="loginImage" />
+          </div> */}
+          <div className="header">
+            <h3>AccordNet</h3>
+            <p>Join the Community</p>
           </div>
           <div className="form">
             <div className="form-group">
-              <label htmlFor="username">UserName</label>
-              <input type="text" name="username" placeholder="username" />
+              {/* <label htmlFor="username">UserName</label> */}
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={this.handleChange.bind(this)}
+              />
             </div>
             <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" placeholder="email" />
+              {/* <label htmlFor="email">Email</label> */}
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={this.handleChange.bind(this)}
+              />
             </div>
             <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              {/* <label htmlFor="password">Password</label> */}
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.handleChange.bind(this)}
+              />
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button
+            type="button"
+            className="btn"
+            onClick={this.handleSubmit.bind(this)}
+          >
             Register
           </button>
         </div>
